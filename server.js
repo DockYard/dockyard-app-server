f17a07de44f50e26c16a01f175198649e4a1abde
Brian Cardarella <brian@dockyard.com>
2016-07-07 22:32:29 -0400

    Added downloader and notifier

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const FastBootAppServer = require('fastboot-app-server');
+const S3Notifier = require('fastboot-s3-notifier');
+const S3Downloader = require('fastboot-s3-downloader');
+const Creds = {
+  distPath: '/home/production/dockyard-app-server/dist',
+  bucket: 'dockyard-com-staging',
+  key: 'dy.json'
+};
 
+let downloader = new S3Downloader(Creds);
+let notifier = new S3Notifier(Creds);
 let server = new FastBootAppServer({
-  distPath: '/Users/bcardarella/projects/dockyard.com/dist'
+  outputPath: '/home/production/dockyard-app-server/dist',
+  downloader,
+  notifier
 });
 
 server.start();
